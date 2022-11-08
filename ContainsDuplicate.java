5cc584cac380832243487c3f6c29bb79ceb22293
AlexKTP <alexandre.malachin@gmail.com>
2022-11-08 06:51:36 +0100

    Contains duplicate

---
+++ ContainsDuplicate.java
@@ -0,0 +1,6 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return set.size() != nums.length;
+    }
+}
