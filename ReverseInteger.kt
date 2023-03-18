62030ee8e415feeddfdb4246a7e69b0faa1d49b3
AlexKTP <alexandre.malachin@gmail.com>
2023-03-18 12:27:02 +0100

    ReverseInteger with more Built-in Function

--- ReverseInteger.kt
+++ ReverseInteger.kt
@@ -1,11 +1,10 @@
 class Solution {
     fun reverse(x: Int): Int {
-        var result: Long = 0
-        var absolute = Math.abs(x)
-        if(absolute<0)return 0
-        if(x<0) result =  absolute.toString().reversed().toLong() *-1
-        else result = x.toString().reversed().toLong()
-        if(result in Int.MIN_VALUE..Int.MAX_VALUE ) return result.toInt()
-        else return 0
+         var coeff = 1
+         if(x<0) coeff = -1
+         val temp = 
+         Math.abs(x).toString().reversed().toIntOrNull() ?: return 
+0
+         return temp*coeff
     }
 }
